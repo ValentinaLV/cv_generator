4c165d6d0d4215915f24b158f0d12a16df23254e
ValentinaLV <tina.lysenok@gmail.com>
2020-03-20 13:44:28 +0200

    implemented logic for resume pdf file generation

--- pdf/urls.py
+++ pdf/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
-from .views import ProfileView, ResumeListView, ResumeView
+from .views import ProfileView
+from .views import ResumeListView, ResumeView
+from .views import GeneratePdf
 
 
 urlpatterns = [
     path('', ProfileView.as_view(), name='profile'),
     path('resumes', ResumeListView.as_view(), name='resumes'),
     path('resume/<str:slug>', ResumeView.as_view(), name='resume-details'),
+    path('download/<str:slug>', GeneratePdf.as_view(), name='resume-download'),
 
 ]
 
--- pdf/views.py
+++ pdf/views.py
@@ -1,9 +1,17 @@
-from django.contrib import messages
-from django.shortcuts import render, redirect
-from django.views.generic.base import View
+import io
 
-from .models import Profile
+import pdfkit
+from django.contrib import messages
+from django.http import FileResponse
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.template import loader
+from django.views.generic.base import View
+from reportlab.pdfgen import canvas
+
 from .forms import ProfileForm
+from .models import Profile
+from .utils import Render
 
 
 class ProfileView(View):
@@ -37,3 +45,49 @@ class ResumeView(View):
         return render(request, 'resume.html', {
             'resume': resume
         })
+
+
+def resume(request, slug):
+    user_profile = Profile.objects.get(url=slug)
+    template = loader.get_template('resume.html')
+    html = template.render({'resume': user_profile})
+    options = {
+        'page-size': 'Letter',
+        'encoding': "UTF-8",
+    }
+    pdf = pdfkit.from_string(html, False, options)
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment'
+    filename = "resume.pdf"
+    return response
+
+
+def some_view(request, slug):
+    # Create a file-like buffer to receive PDF data.
+    buffer = io.BytesIO()
+
+    # Create the PDF object, using the buffer as its "file."
+    p = canvas.Canvas(buffer)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    p.drawString(100, 100, f"Hello world. + {slug}")
+
+    # Close the PDF object cleanly, and we're done.
+    p.showPage()
+    p.save()
+
+    # FileResponse sets the Content-Disposition header so that browsers
+    # present the option to save the file.
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
+
+
+class GeneratePdf(View):
+    def get(self, request, slug):
+        user_profile = Profile.objects.get(url=slug)
+        params = {
+            'resume': user_profile,
+            'request': request
+        }
+        return Render.render('resume.html', params)
